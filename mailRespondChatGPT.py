9ae474ca28ee09880ccf34ba620851234dceb33f
Krapanuk <46630690+Krapanuk@users.noreply.github.com>
2024-04-06 23:55:31 +0200

    Update mailRespondChatGPT.py: Bugfix avoiding the same mail beeing multiple processed

--- mailRespondChatGPT.py
+++ mailRespondChatGPT.py
@@ -2,11 +2,15 @@ import imaplib
 import email
 from email.header import decode_header
 import requests
+import time # for delays
 from credentials import *
 from mailSend import *
 
 # Here you can personalize the AIs responses
 whoAmI= 'You are a helpful assistant answering the mails in the role of a 40 year old software developer, married with a beautiful wife. You have 2 kids and live in Germany near Bielefeld.' 
+
+def log_message(message):
+    print(message)
 
 def api_call(request_data, api_key):
     url = 'https://api.openai.com/v1/chat/completions'
@@ -35,6 +39,8 @@ messages = messages[0].split()
 
 for mail_id in messages:
     # Fetch the email by ID
+    if mail_id in processed_ids:
+        continue  # Don't process the same mail twice
     status, data = mail.fetch(mail_id, '(RFC822)')
     for response_part in data:
         if isinstance(response_part, tuple):
@@ -64,9 +70,12 @@ for mail_id in messages:
             if api_response:
                 answer = api_response['choices'][0]['message']['content']
                 mailSend(answer)
+                log_message(f"Sent response for mail ID {mail_id}")
     
             # Mark the email for deletion
             mail.store(mail_id, '+FLAGS', '\\Deleted')
+            processed_ids.add(mail_id)  # mark mail as processed
+            time.sleep(1)  # delay, giving the server enough time to process the request
 
 # Expunge and logout
 mail.expunge()
